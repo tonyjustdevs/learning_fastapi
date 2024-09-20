743397ee1956df7d8fba740c51c64c9156a1f5c6
tonyjustdevs <tonyjustdevs@gmail.com>
2024-09-20 17:52:40 +0700

    add user classes based on pydantic base model

--- main.py
+++ main.py
@@ -8,9 +8,21 @@ print_start_info(__file__)
 ##############################################################################
 
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import Optional,List
 app = FastAPI()
 
+
+# Schemas
+class User1a_cls(BaseModel): # resolve #3
+    u1a_score_attr: Optional[int] = None
+    u1a_weapons_attr: Optional[List[str]] = None
+class User1_cls(User1a_cls):
+    u1_login_attr: int
+    u1_country_attr: Optional[str] = None
+
+
+# Routes
 @app.get("/")
 def root_fn():
     return "G'day mate 🦘🦘🦘!"
@@ -22,3 +34,10 @@ def root_fn():
 ############################## END CODE HERE #################################
 ##############################################################################
 print_end_info(__file__)  
+
+
+# DONE [2] Build Root GET endpoint ("/") #2
+# DONE  [3] Create U1 and U1a User classes (pydantic models) #3
+# TODO [4] Create U1 and U1a User sample data (userid=0) #4
+# TODO [5] Build Default User Get endpoint with ("/user/0", pydantic response model) #5
+# TODO [6] Build Path Parameter User Get endpoint ("/user/{userid}") #6
