bdb31a5da928df4f3b7d9af18ad38ed501325f37
tonyjustdevs <tonyjustdevs@gmail.com>
2024-11-25 07:57:21 +0700

    cleanup and make neat main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ print_start_info(__file__)
 ############################## START CODE HERE ###############################
 ##############################################################################
 
+
 from fastapi import FastAPI, Request  
-from pydantic import BaseModel
-from typing import Optional, List
 from app.routes.users import user_router
 from exceptions import CustomException
 from fastapi.responses import JSONResponse
+
 app = FastAPI()
 app.include_router(user_router)
 
@@ -24,48 +24,7 @@ def custom_exception_handler(req: Request, exc: CustomException):
   )
 
 
-
-
 ##############################################################################
 ############################## END CODE HERE #################################
 ##############################################################################
-print_end_info(__file__)  
-
-
-# aTODO [#11 Build Simple Fast API App]  [#12/fastapi]
-# aTODO [#14 Build PUT endpoint ("/user/{userid}")]  [#11/fastapi]
-
-
-# aTODO [#15 Build DELETE endpoint ("/user/{userid}")]  [#11/fastapi]
-# aTODO [#16 Refactor to Async Functions]  [#11/fastapi]
-# aTODO [#17 Refactor App Directory Structure]  [#11/fastapi]
-# aTODO [#18 Add API Routers]  [#11/fastapi]
-# aTODO [#19 Add HTTP Exception]  [#11/fastapi]
-# aTODO [#20 Add Exception Class]  [#11/fastapi]
-# aTODO [#21 Add Logging]  [#11/fastapi]
-# aTODO [#22 Add Custom Exception Handlers]  [#11/fastapi]
-# aTODO [#23 Add Headers and Dependencies]  [#11/fastapi]
-
-
-
-#zDONE [2] Build Root GET endpoint ("/") #2
-#zDONE [3] Create U1 and U1a User classes (pydantic models) #3
-#zDONE [4] Create U1 and U1a User sample data (userid=0) #4
-#zDONE [5] Build Default User Get endpoint with ("/user/0", pydantic response model) #5
-#zDONE [6] Build Path Parameter User Get endpoint ("/user/{userid}") #6
-#zDONE [#24 1. Accepts post body request (u1 class) [#12]]  [#12/fastapi]
-#zDONE [#25 2. create new user of U1 class [#12]]  [#12/fastapi]
-#zDONE [#26 2a. creates new user id [#12]]  [#12/fastapi]
-#zDONE [#27 2b. creates new user in sample dicts [#12]]  [#12/fastapi]
-#zDONE [#28 3. return u2 class post response [#12]]  [#12/fastapi]
-#zDONE [#29 bug fix]
-#zDONE [#12 Create U2 POST Response class (pydantic model)]  [#11/fastapi]
-#zDONE [#13 Build Query Parameters GET endpoint ({start},{limit},"/user/{userid}")]  [#11/fastapi]
-#zDONE [#33 create new query_parameters endpoint u3_endpt [13]]  [#13/fastapi]
-#zDONE [#34 create start and limit parameters  [13]]  [#13/fastapi]
-#zDONE [#35 create new u3 class - list of u1 [13]]  [#13/fastapi]
-#zDONE [#36 create u3_endpt return u3_class [13]]  [#13/fastapi]
-#zDONE [#41 update function [create_new_u1_user_fn] to optionally input [userid] (#14)]  [#14/fastapi]
-#zDONE [#42 create user: if user not exist [userid] (#14)]  [#14/fastapi]
-#zDONE [#43 update user: if user exists (dicts) (#14)]  [#14/fastapi]
-#zDONE [#44 update PUT endpoint (#14)]  [#14/fastapi]
+print_end_info(__file__)
